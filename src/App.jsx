c15826e2f6b91f7ed0b61d4a692e44d2cd45d47e
ivanvarella <ivanvarella@gmail.com>
2023-08-11 14:55:24 -0300

    Create Login/Register pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import "./App.css";
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-//Pages
-import Home from "./pages/Home/Home";
-import About from "./pages/About/About";
-
 //Components
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+
+//Pages
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
             <Route path="/about" element={<About />} />
           </Routes>
         </div>
