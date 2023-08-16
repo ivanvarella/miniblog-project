9af31e4314bc75f9d29d3260748daa137a7fd463
ivanvarella <ivanvarella@gmail.com>
2023-08-16 09:45:27 -0300

    Register user on Firebase

--- src/hooks/useAuthentication.jsx
+++ src/hooks/useAuthentication.jsx
@@ -1,3 +1,5 @@
+import { db } from "../firebase/config";
+
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -23,4 +25,54 @@ export const useAuthentication = () => {
       return;
     }
   }
+
+  const createUser = async (data) => {
+    checkIfIsCancelled();
+
+    setLoading(true);
+    setError(null);
+
+    try {
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+      await updateProfile(user, {
+        displayName: data.displayName,
+      });
+
+      setLoading(false);
+
+      return user;
+    } catch (error) {
+      console.log(error.message);
+      console.log(typeof error.message);
+
+      let systemErrorMessage;
+
+      if (error.message.includes("Password")) {
+        systemErrorMessage = "A senha deve conter no mínimo 6 caracteres.";
+      } else if (error.message.includes("email-already")) {
+        systemErrorMessage = "Esse email já está em uso.";
+      } else {
+        systemErrorMessage =
+          "Ocorreu um erro inesperado. Por favor tente mais tarde.";
+      }
+
+      setLoading(false);
+      setError(systemErrorMessage);
+    }
+  };
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+
+  return {
+    auth,
+    createUser,
+    error,
+    loading,
+  };
 };
