a972194fe08da7d0519009801d98097c4e9b2499
ivanvarella <ivanvarella@gmail.com>
2023-08-17 09:42:53 -0300

    Login function

--- src/hooks/useAuthentication.jsx
+++ src/hooks/useAuthentication.jsx
@@ -73,6 +73,33 @@ export const useAuthentication = () => {
     signOut(auth);
   };
 
+  //Login - sign in
+  const login = async (data) => {
+    checkIfIsCancelled();
+
+    setLoading(true);
+    setError(false);
+
+    try {
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+      setLoading(false);
+    } catch (error) {
+      let systemErrorMessage;
+
+      if (error.message.includes("user-not-found")) {
+        systemErrorMessage = "Usuário não encontrado.";
+      } else if (error.message.includes("wrong-password")) {
+        systemErrorMessage = "Senha incorreta.";
+      } else {
+        systemErrorMessage =
+          "Ocorreu um erro inesperado. Por favor tente mais tarde.";
+      }
+
+      setLoading(false);
+      setError(systemErrorMessage);
+    }
+  };
+
   useEffect(() => {
     return () => setCancelled(true);
   }, []);
@@ -83,5 +110,6 @@ export const useAuthentication = () => {
     error,
     loading,
     logout,
+    login,
   };
 };
