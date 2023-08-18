dfee3101f03251519c1b4b01fa39ab454d91ee38
ivanvarella <ivanvarella@gmail.com>
2023-08-18 16:17:43 -0300

    Estruture Dashboard page

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,34 @@
 import styles from "./Dashboard.module.css";
 
+import { Link } from "react-router-dom";
+
+//Hooks
+import { useAuthValue } from "../../contexts/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
 const Dashboard = () => {
+  const { user } = useAuthValue();
+  const uid = user.id;
+
+  //User posts
+  const posts = [];
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h2>Dashboard</h2>
+      <p>Gerencie seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div>
+          <p>Vocé ainda não possui posts.</p>
+          <Link to="/posts/create" className="btn">
+            Criar primeiro post
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <p>Tem Posts!!!!!</p>
+        </div>
+      )}
     </div>
   );
 };
